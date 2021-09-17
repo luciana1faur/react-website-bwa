3990742cd39581709882ae296fc01495ea75ebb8
Faur Luciana <luciana_faur@yahoo.com>
2021-09-17 16:23:21 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+/* !!! After this tutorial https://www.youtube.com/watch?v=I2UBjN5ER4s este salvat sub denumirea C:/USERS/LUCIA/REACT-WEBSITE-BWA!!!*/
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+export default App;
